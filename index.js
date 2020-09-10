d79255177e79cb5d82c5e53c452c3920d6a21fd1
Hendro Steven <hendro.steven@gmail.com>
2020-09-10 15:15:52 +0700

    update response

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
-    var body = "<h1>Welcome to NodeJS</h1>";
+    var body = "<h1>Selamat datang di NodeJS Programming</h1>";
     res.writeHead(200, {
         'Content-Type': 'text/html',
         'Content-Length': body.length
